aa14cdbd9e69cf5f2992a2d3e63a5aa8fb36972d
mikiyoshi <mikiyoshi.kokura@gmail.com>
2021-12-16 22:27:38 +0000

    submit project

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -10,10 +10,7 @@ $(document).ready(function () {
     if (backupArr.length === 1) {
       console.log('backup: ', backupArr[backupArr.length - 1]);
       for (let obj of tweets) {
-        // console.log('OBJ: ', obj);
-        // console.log('OBJ: ', obj.content.text);
         let tweet = createTweetElement(obj);
-        // console.log('OBJ: ', tweet);
         $('.messageBox').append(tweet);
       }
     } else {
@@ -24,21 +21,10 @@ $(document).ready(function () {
         $('.messageBox').prepend(tweet);
       }
     }
-    // console.log('backup2: ', backupArr[0][0].content.text);
-    // console.log('TWEETS: ', tweets);
-    // loops through tweets
-    // calls createTweetElement for each tweet
-    // takes return value and appends it to the tweets container
   };
 
   const createTweetElement = function (tweet) {
-    // console.log('createTweet: ', tweet.content.text);
-    // const backupArr = [];
-    // if(tweet.content.text === )
-    // return backupArr
-    // console.log('createTweet: ', tweet.user.avatars);
-    // console.log('timenumber: ', timeago.format(1461113959088));
-    // console.log('time: ', tweet.created_at);
+    /* Your code for creating the tweet element */
     let $tweet = $(`
           <div class="userCard">
             <div class="userCardBody">
@@ -75,30 +61,23 @@ $(document).ready(function () {
               </div>
             </div>
           </div>
-  `); /* Your code for creating the tweet element */
-    // ...
+  `);
     return $tweet;
   };
 
   // renderTweets(data);
 
   const loadTweets = function () {
-    // console.log($button);
     console.log('Button clicked, performing ajax call...');
     $.ajax('/tweets', { method: 'GET' }).then(function (jsonData) {
       const newOrder = jsonData.reverse();
       backupArr.push(newOrder);
       renderTweets(newOrder);
-      // newData.push(jsonData);
-      // console.log('jsonData', newOrder);
-      // $button.replaceWith(morePostsHtml);
     });
   };
-  // console.log('after Data', newData);
 
   $('form').submit(function (event) {
     event.preventDefault();
-    // console.log('test test');
     const str = $('form').serialize();
     if ($('#tweet-text').val() === '') {
       $('label')
@@ -106,8 +85,6 @@ $(document).ready(function () {
         .addClass('alert')
         .text(" You can't TWEET empty message")
         .show();
-      // $('#resultPost').text("You can't TWEET empty message").show();
-      // console.log(str);
     } else if ($('#tweet-text').val().length > 140) {
       $('label')
         .siblings('#resultPost')
@@ -120,10 +97,8 @@ $(document).ready(function () {
         .addClass('alert')
         .text('Please click TWEET button')
         .show();
-      // console.log($('#tweet-text').val().length);
       $('#tweet-text').val('');
     }
-    // console.log(str);
 
     $.post(
       '/tweets', // url
@@ -133,18 +108,4 @@ $(document).ready(function () {
       loadTweets(str);
     });
   });
-
-  // $.ajax({
-  //   url: url,
-  //   method: 'GET',
-  // })
-  //   .done((result) => {
-  //     console.log(result);
-  //   })
-  //   .fail((error) => {
-  //     console.log(`Error: ${error.message}`);
-  //   })
-  //   .always(() => {
-  //     console.log('request to TV Maze done');
-  //   });
 });
